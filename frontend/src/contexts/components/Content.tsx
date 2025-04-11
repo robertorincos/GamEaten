289cf7a8ec1b38f99e19c8662e7f007985625745
Bobertkiller <felipe.mazzeo.barbosa@outlook.com>
2025-04-11 19:57:40 -0300

    Login e Signup

---
+++ frontend/src/contexts/components/Content.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import AutoFixHighRoundedIcon from '@mui/icons-material/AutoFixHighRounded';
+import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded';
+import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
+import { SitemarkIcon } from './CustomIcons';
+
+const items = [
+  {
+    icon: <SettingsSuggestRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Comunity made by Gamers for Gamers',
+    description:
+      '...',
+  },
+  {
+    icon: <ConstructionRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Rate your Favorite Games',
+    description:
+      '...',
+  },
+  {
+    icon: <ThumbUpAltRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Great user experience',
+    description:
+      '...',
+  },
+  {
+    icon: <AutoFixHighRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Easy to Use',
+    description:
+      ' ...',
+  },
+];
+
+export default function Content() {
+  return (
+    <Stack
+      sx={{ flexDirection: 'column', alignSelf: 'center', gap: 4, maxWidth: 450 }}
+    >
+      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+        <SitemarkIcon />
+      </Box>
+      {items.map((item, index) => (
+        <Stack key={index} direction="row" sx={{ gap: 2 }}>
+          {item.icon}
+          <div>
+            <Typography gutterBottom sx={{ fontWeight: 'medium' }}>
+              {item.title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {item.description}
+            </Typography>
+          </div>
+        </Stack>
+      ))}
+    </Stack>
+  );
+}
